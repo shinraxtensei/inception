2d3a5f12f5d710673705268ea313f04d1353ff5c
shinraxtensei <anasshouarii@gmail.com>
2023-01-31 12:18:02 -0500

    added some code

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,9 +1,9 @@
-# #!/bin/bash
+#!/bin/bash
 
 sed -i "s/bind-address.*/bind-address = 0.0.0.0/g"  /etc/mysql/mariadb.conf.d/50-server.cnf
 
 
-# TODO : try to import the database from wordpress \ or try to fix the database connection 
+# # TODO : try to import the database from wordpress \ or try to fix the database connection 
 
 
 
@@ -12,55 +12,89 @@ sed -i "s/bind-address.*/bind-address = 0.0.0.0/g"  /etc/mysql/mariadb.conf.d/50
 
 
 
-# service mysql start 
+# # service mysql start 
 
-# echo "
-# # -- Delete Already Existing Root User on the Host
-# DELETE FROM
-#   mysql.user
-# WHERE
-#   User = '$MARIADB_ADMIN_USER'
-#   AND Host NOT IN ('$HOST_NAME', '$HOST_IPV4', '$HOST_IPV6');
+# # echo "
+# # # -- Delete Already Existing Root User on the Host
+# # DELETE FROM
+# #   mysql.user
+# # WHERE
+# #   User = '$MARIADB_ADMIN_USER'
+# #   AND Host NOT IN ('$HOST_NAME', '$HOST_IPV4', '$HOST_IPV6');
 
-# # -- Set Password of Root User on MariaDB
-# SET
-#   PASSWORD FOR '$MARIADB_ADMIN_USER'@'$HOST_NAME' = PASSWORD('$MARIADB_ADMIN_PWD');
+# # # -- Set Password of Root User on MariaDB
+# # SET
+# #   PASSWORD FOR '$MARIADB_ADMIN_USER'@'$HOST_NAME' = PASSWORD('$MARIADB_ADMIN_PWD');
 
-# # -- Create WordPress Database
-# CREATE DATABASE IF NOT EXISTS $MARIADB_DB;
+# # # -- Create WordPress Database
+# # CREATE DATABASE IF NOT EXISTS $MARIADB_DB;
 
-# # -- Create Another User for WordPress
-# CREATE USER '$MARIADB_USER'@'%' IDENTIFIED BY '$MARIADB_PWD';
+# # # -- Create Another User for WordPress
+# # CREATE USER '$MARIADB_USER'@'%' IDENTIFIED BY '$MARIADB_PWD';
 
-# # -- Grant Permissions
-# GRANT ALL PRIVILEGES ON $MARIADB_DB.* TO '$MARIADB_USER'@'%' WITH GRANT OPTION;
+# # # -- Grant Permissions
+# # GRANT ALL PRIVILEGES ON $MARIADB_DB.* TO '$MARIADB_USER'@'%' WITH GRANT OPTION;
 
-# # -- Apply
-# FLUSH PRIVILEGES;" > my_sql_file.sql
+# # # -- Apply
+# # FLUSH PRIVILEGES;" > my_sql_file.sql
 
-# mysql < my_sql_file.sql
+# # mysql < my_sql_file.sql
 
 
-#!/bin/bash
-if [ "$(ls -A /var/lib/mysql)" ];then
-    service mysql start 2> /dev/null;
-fi
 
-#mysql -p$MYSQL_ROOT_PASSWORD -e ""
-if [ $? -ne 0 ] || [ ! -d /var/lib/mysql/wordpress ]; then
-    echo "Need to config wordpress DB"
-    killall mysqld
-    mysql_install_db &> /dev/null;
-    service mysql start 2> /dev/null;
+# if [ "$(ls -A /var/lib/mysql)" ];then
+#     service mysql start 2> /dev/null;
+# fi
 
-    mysql -e "DELETE FROM mysql.user WHERE User='';";
-    mysql -e "CREATE DATABASE wordpress;";
-    mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';";
-    mysql -e "GRANT ALL PRIVILEGES ON wordpress.* TO '$MYSQL_USER'@'%' WITH GRANT OPTION;";
-    mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';FLUSH PRIVILEGES;";
-else
-    echo "Wordpress DB exist already.";
-fi
+# #mysql -p$MYSQL_ROOT_PASSWORD -e ""
+# if [ $? -ne 0 ] || [ ! -d /var/lib/mysql/wordpress ]; then
+#     echo "Need to config wordpress DB"
+#     killall mysqld
+#     mysql_install_db &> /dev/null;
+#     service mysql start 2> /dev/null;
 
-killall mysqld
-mysqld
+#     mysql -e "DELETE FROM mysql.user WHERE User='';";
+#     mysql -e "CREATE DATABASE '$MYSQL_DATABASE';";
+#     mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';";
+#     mysql -e "GRANT ALL PRIVILEGES ON wordpress.* TO '$MYSQL_USER'@'%' WITH GRANT OPTION;";
+#     mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';FLUSH PRIVILEGES;";
+# else
+#     echo "Wordpress DB exist already.";
+# fi
+
+
+
+
+
+
+service mysql start 
+
+
+# echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE ;" > db1.sql
+# echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;" >> db1.sql
+# echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' ;" >> db1.sql
+# echo "ALTER USER 'root'@'' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' ;" >> db1.sql
+# echo "FLUSH PRIVILEGES;" >> db1.sql
+
+
+
+
+echo "DELETE FROM mysql.user WHERE User='';" > db.sql
+echo "CREATE DATABASE $MYSQL_DATABASE;"> db.sql
+echo "CREATE USER IF NOT EXISTS $MYSQL_USER@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" > db.sql
+echo "GRANT ALL PRIVILEGES ON wordpress.* TO $MYSQL_USER@'%' WITH GRANT OPTION;" > db.sql
+# echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';FLUSH PRIVILEGES;" > db.sql
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY $MYSQL_ROOT_PASSWORD;" > db.sql
+echo "FLUSH PRIVILEGES;" > db.sql""
+mysql < db.sql
+
+# mysqld
+# kill $(cat /var/run/mysqld/mysqld.pid)
+
+# mysqld
+
+
+# killall mysqld
+
+
+bash
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,44 +1,8 @@
 # #!/bin/bash
-
-#     mkdir -p /var/www/html
-#     cd /var/www/html
-#     rm -rf *
-
-#     curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-#     chmod +x wp-cli.phar
-#     mv wp-cli.phar /usr/local/bin/wp
-#     wp core download --allow-root
-
-#     # wget http://wordpress.org/latest.tar.gz 
-# 	# tar xfz latest.tar.gz
-#     # rm -rf latest.tar.gz
-#     # mv wordpress /var/www/html/wordpress
-
-#     mv wp-config-sample.php wp-config.php
-#     sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config.php
-#     sed -i "s/username_here/$MYSQL_USER/g" wp-config.php
-#     sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config.php
-
-#     sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
-
-    
-#     wp core install --url=$DOMAIN_NAME/ --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
-#     wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
-#     wp theme install astra --activate --allow-root
-
-#     mkdir /run/php
-
-#     # # service php7.3-fpm start
-#     # /usr/sbin/php-fpm7.3 -F
-
-# **********************************************************************************************************************
-
-
-#!/bin/bash
-
     mkdir -p /var/www/html
     cd /var/www/html
-    rm -rf *
+    
+    # rm -rf *
 
     curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
     chmod +x wp-cli.phar
@@ -54,17 +18,33 @@
     sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config.php
     sed -i "s/username_here/$MYSQL_USER/g" wp-config.php
     sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config.php
+    sed -s "s/localhost/$WP_DBHOST/g" wp-config.php
 
     sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
 
 
     # TODO : fix the connection to the database 
 
-    wp core install --url=$DOMAIN_NAME/ --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
-    wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
-    wp theme install astra --activate --allow-root
+
+    wp core install --allow-root --url="$WP_URL" --title="$WP_TITLE" --admin_user="$WP_ADMIN" --admin_password="$WP_ADMIN_PWD" --admin_email="$WP_ADMIN_EMAIL" --skip-email
+    echo "WP admin user = $WP_ADMIN"
+    echo "WP password for admin user = $WP_ADMIN_PWD"
+
+    wp user create $WP_USER $WP_USER_EMAIL  --user_pass=$WP_USER_PWD --role=author --allow-root
+    echo "WP user created, login=$WP_USER & password=$WP_USER_PWD"
+
+     echo "Wordpress installed & configured with Mariadb"
+
+
+     
+    # wp core install --url=$DOMAIN_NAME/ --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
+    # wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
+    # wp theme install astra --activate --allow-root
 
     mkdir /run/php
 
     # service php7.3-fpm start
     # /usr/sbin/php-fpm7.3 -F
+
+    php-fpm7.3 --nodaemonize
+    # bash
