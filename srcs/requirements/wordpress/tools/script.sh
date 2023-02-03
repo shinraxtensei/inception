f8f9c8aab9a04439c866b9099087e66e0d2d3f8d
shinra <shinra@shinra.ahouari42>
2023-02-03 11:15:43 -0500

    added more on redis but still facing a problem with the filesystm not being writable

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,9 +1,11 @@
 # #!/bin/bash
     mkdir -p /var/www/html
+    chown -R www-data:www-data /var/www/html
+    chmod 777 /var/www/html
     cd /var/www/html
     
     rm -rf *
-    
+    mkdir /run/php
     curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
     chmod +x wp-cli.phar
     mv wp-cli.phar /usr/local/bin/wp
@@ -20,28 +22,42 @@
     
 
 
-wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$WP_DBHOST --allow-root --skip-check --extra-php <<PHP
-    define('WP_REDIS_HOST', 'redis');
-    define( 'WP_REDIS_HOST', '127.0.0.0' );
-    define('WP_REDIS_PORT', 6379);
-    define('WP_REDIS_DISABLED', false);
-    define( 'WP_REDIS_TIMEOUT', 1 );
-    define( 'WP_REDIS_READ_TIMEOUT', 1 );
-    define( 'WP_REDIS_DATABASE', 0 );
-PHP
+    # define('WP_REDIS_HOST', 'redis');
+    # define( 'WP_REDIS_HOST', '127.0.0.0' );
+    # define('WP_REDIS_PORT', 6379);
+    # define('WP_REDIS_DISABLED', false);
+    # define( 'WP_REDIS_TIMEOUT', 1 );
+    # define( 'WP_REDIS_READ_TIMEOUT', 1 );
+    # define( 'WP_REDIS_DATABASE', 0 );
 
+
+# wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$WP_DBHOST --allow-root --skip-check --extra-php <<PHP
+
+# # define('WP_REDIS_DISABLED', false);
+# # define( 'WP_REDIS_HOST', 'redis');
+# # define( 'WP_REDIS_PORT', 6379 );
+
+
+# PHP
+    wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$WP_DBHOST --allow-root --skip-check
 
 
     wp core install --allow-root --url="$WP_URL" --title="$WP_TITLE" --admin_user="$WP_ADMIN" --admin_password="$WP_ADMIN_PWD" --admin_email="$WP_ADMIN_EMAIL" --skip-email
     wp user create $WP_USER $WP_USER_EMAIL  --user_pass=$WP_USER_PWD --role=author --allow-root
+   
 
-    service php7.3-fpm start
+    wp plugin install redis-cache --activate --allow-root
 
-    wp plugin install redis-cache --activate  --allow-root
+
+    wp config set WP_REDIS_HOST redis --type=constant --allow-root
+    wp config set WP_REDIS_PORT 6379 --type=constant --allow-root
+    wp config set WP_REDIS_DISABLED false --type=constant --allow-root
+
+
+     cat wp-config.php
     wp plugin update --all --allow-root
     wp redis enable --allow-root
     
-    service php7.3-fpm stop
-    mkdir /run/php
+
 
     php-fpm7.3 --nodaemonize
