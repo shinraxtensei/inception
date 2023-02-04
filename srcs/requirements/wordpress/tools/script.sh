7c03e17d13c19dd7fac09b2fd37bd2883d4dae80
shinraxtensei <chinraxtensei@gmail.com>
2023-02-04 10:00:35 -0500

    adminer done

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -9,28 +9,8 @@
     chmod +x wp-cli.phar
     mv wp-cli.phar /usr/local/bin/wp
     wp core download --path=/var/www/html --allow-root
-    # chown -R www-data:www-data /var/www/html
 
     sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf 
-    # mv wp-config-sample.php wp-config.php
-    # sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config.php
-    # sed -i "s/username_here/$MYSQL_USER/g" wp-config.php
-    # sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config.php
-    # sed -i "s/localhost/$WP_DBHOST/g" wp-config.php
-
-
-    
-
-
-    # define('WP_REDIS_HOST', 'redis');
-    # define('WP_REDIS_PORT', 6379);
-    # define('WP_REDIS_DISABLED', false);
-    # define( 'WP_REDIS_TIMEOUT', 1 );
-    # define( 'WP_REDIS_READ_TIMEOUT', 1 );
-    # define( 'WP_REDIS_DATABASE', 0 );
-
-
-
 
 wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$WP_DBHOST --path=/var/www/html --allow-root --skip-check --extra-php <<PHP
 
@@ -39,65 +19,22 @@ define('WP_REDIS_HOST', 'redis');
 define('WP_REDIS_PORT', 6379);
 define('WP_REDIS_DISABLED', false);
 define( 'WP_REDIS_TIMEOUT', 1 );
-define( 'WP_REDIS_READ_TIMEOUT', 1 );
 
 PHP
-
-# wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$WP_DBHOST --path=/var/www/html --allow-root --skip-check
-
-# wp config set WP_REDIS_HOST redis --type=constant  --path=/var/www/html --allow-root
-# wp config set WP_REDIS_PORT 6379 --type=constant  --path=/var/www/html --allow-root
-# wp config set WP_REDIS_DISABLED false --type=constant  --path=/var/www/html --allow-root
 
 
 wp core install --allow-root --url="$WP_URL" --title="$WP_TITLE" --admin_user="$WP_ADMIN" --admin_password="$WP_ADMIN_PWD" --admin_email="$WP_ADMIN_EMAIL"  --path=/var/www/html --skip-email
 wp user create wp user create $WP_USER $WP_USER_EMAIL  --user_pass=$WP_USER_PWD --role=author  --path=/var/www/html --allow-root
 
 
-# service php7.3-fpm start
+
 
 wp plugin install redis-cache --activate  --path=/var/www/html --allow-root
-# wp plugin install redis-cache --path=/var/www/html --allow-root
-# wp plugin activate redis-cache --path=/var/www/html --allow-root
 
 wp plugin update --all  --path=/var/www/html --allow-root
 
 wp redis enable --path=/var/www/html --allow-root
-# service php7.3-fpm stop
 
  cat /var/www/html/wp-config.php
-# php-fpm7.3 -F
-
-
-
-# wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$WP_DBHOST --path=/var/www/html --allow-root --skip-check --extra-php <<PHP
-
-# # define('WP_REDIS_DISABLED', false);
-# # define( 'WP_REDIS_HOST', 'redis');
-# # define( 'WP_REDIS_PORT', 6379 );
-
-
-# PHP
-#     wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$WP_DBHOST --path=/var/www/html --allow-root --skip-check
-
-
-#     wp core install --allow-root --url="$WP_URL" --title="$WP_TITLE" --admin_user="$WP_ADMIN" --admin_password="$WP_ADMIN_PWD" --admin_email="$WP_ADMIN_EMAIL"  --path=/var/www/html --skip-email
-#     wp user create $WP_USER $WP_USER_EMAIL  --user_pass=$WP_USER_PWD --role=author  --path=/var/www/html --allow-root
-   
-
-#     # wp plugin install redis-cache --activate  --path=/var/www/html --allow-root
-
-#     wp plugin install redis-cache --path=/var/www/html --allow-root
-#     wp plugin activate redis-cache --path=/var/www/html --allow-root
-
-#     # wp config set WP_REDIS_HOST redis --type=constant  --path=/var/www/html --allow-root
-#     # wp config set WP_REDIS_PORT 6379 --type=constant  --path=/var/www/html --allow-root
-#     # wp config set WP_REDIS_DISABLED false --type=constant  --path=/var/www/html --allow-root
-
-#     wp plugin update --all  --path=/var/www/html --allow-root
-#     wp redis enable --path=/var/www/html --allow-root 
-    
-
-   
 
     php-fpm7.3 --nodaemonize
