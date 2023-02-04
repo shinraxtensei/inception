2d362566d929c6c1f78c2327b13ad90994f31513
shinraxtensei <chinraxtensei@gmail.com>
2023-02-04 07:45:55 -0500

    done with ftp

---
+++ srcs/requirements/bonus/ftp/tools/configure.sh
@@ -0,0 +1,24 @@
+# #!/bin/bash
+
+service vsftpd start
+
+adduser hamid --disabled-password --gecos ""
+echo "hamid:hamid" | chpasswd
+
+echo "hamid" >> /etc/vsftpd.user_list
+
+mkdir /home/hamid/ftp
+chown -R hamid:hamid /home/hamid/ftp
+
+sed -i 's/#local_enable=YES/local_enable=YES/g' /etc/vsftpd.conf
+sed -i 's/#chroot_local_user=YES/chroot_local_user=YES/g' /etc/vsftpd.conf
+
+
+echo "
+local_enable=YES
+allow_writeable_chroot=YES
+pasv_enable=YES
+local_root=/home/hamid/ftp
+pasv_min_port=30000
+pasv_max_port=30010
+userlist_file=/etc/vsftpd.userlist" >> /etc/vsftpd.conf
